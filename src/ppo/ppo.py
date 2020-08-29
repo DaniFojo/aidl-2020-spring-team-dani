b3fdf3dcb760ff176056ca91df43ac792eed2d96
Abraham Alcaina <a.alcaina@flexidao.com>
2020-08-29 10:59:53 +0200

    feat: parametrize all hyperparameters

--- src/ppo/ppo.py
+++ src/ppo/ppo.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 import time
+from datetime import datetime
 
 from torch.utils.tensorboard import SummaryWriter
 from torch.distributions import Categorical
@@ -22,11 +23,7 @@ max_steps = 500
 critic_hidden_size = 256
 actor_hidden_size = 16
 render = False
-# eps = np.finfo(np.float32).eps.item()  # machine epsilon
-eps = 1e-5
-coeficient_entropy = 0.01
-coeficient_value = 0.5
-lmbda = 0.95
+mini_batch = 32
 
 
 # Setup device
@@ -43,7 +40,8 @@ def init_normal(m):
 def init_orthogonal(m):
     if type(m) == nn.Linear:
         nn.init.orthogonal_(m.weight)
-
+        if m.bias is not None:
+            m.bias.data.fill_(0)
 # Memory
 
 
@@ -65,7 +63,7 @@ class Memory:
 
 # Actor
 class Actor(nn.Module):
-    def __init__(self, num_of_observations, posibles_actions, hidden_size=actor_hidden_size):
+    def __init__(self, num_of_observations, posibles_actions, hidden_size=actor_hidden_size, initialization=None):
         super(Actor, self).__init__()
         self.num_of_observations = num_of_observations
         self.actor = nn.Sequential(
@@ -76,7 +74,10 @@ class Actor(nn.Module):
             nn.Linear(hidden_size, posibles_actions),
             nn.Softmax(dim=-1)
         )
-        self.actor.apply(init_orthogonal)
+        if initialization == "normal":
+            self.actor.apply(init_normal)
+        if initialization == "orthogonal":
+            self.actor.apply(init_orthogonal)
 
     def forward(self, x):
         probs = self.actor(x)
@@ -105,7 +106,7 @@ class Actor(nn.Module):
 
 # Critic
 class Critic(nn.Module):
-    def __init__(self, num_of_observations, posibles_actions, hidden_size=critic_hidden_size):
+    def __init__(self, num_of_observations, posibles_actions, hidden_size=critic_hidden_size, initialization=None):
         super(Critic, self).__init__()
         self.num_of_observations = num_of_observations
         self.critic = nn.Sequential(
@@ -115,7 +116,11 @@ class Critic(nn.Module):
             nn.Tanh(),
             nn.Linear(hidden_size, 1),
         )
-        self.critic.apply(init_orthogonal)
+
+        if initialization == "normal":
+            self.critic.apply(init_normal)
+        if initialization == "orthogonal":
+            self.critic.apply(init_orthogonal)
 
     def forward(self, x):
         probs = self.actor(x)
@@ -138,6 +143,11 @@ class PPO:
                  num_of_observations,
                  posibles_actions,
                  environment_name,
+                 eps,
+                 coeficient_entropy,
+                 coeficient_value,
+                 weight_decay,
+                 lmbda,
                  actor_hidden_size=actor_hidden_size,
                  critic_hidden_size=critic_hidden_size,
                  pi_lr=pi_lr,
@@ -148,7 +158,9 @@ class PPO:
                  previousTrainedPolicy=None,
                  previousTrainedCritic=None,
                  actorGradientNormalization=0,
-                 observationNormalization=False
+                 observationNormalization=False,
+                 initialization=None,
+                 advantageAlgorithm=None
                  ):
         # Current
         self.actor = Actor(num_of_observations=num_of_observations,
@@ -163,18 +175,18 @@ class PPO:
 
         # Old
         self.actor_old = Actor(num_of_observations=num_of_observations,
-                               posibles_actions=posibles_actions, hidden_size=actor_hidden_size).to(device)
+                               posibles_actions=posibles_actions, hidden_size=actor_hidden_size, initialization=initialization).to(device)
         self.critic_old = Critic(num_of_observations=num_of_observations,
-                                 posibles_actions=posibles_actions, hidden_size=critic_hidden_size).to(device)
+                                 posibles_actions=posibles_actions, hidden_size=critic_hidden_size, initialization=initialization).to(device)
 
         self.actor_old.load_state_dict(self.actor.state_dict())
         self.critic_old.load_state_dict(self.critic.state_dict())
 
         # Optimizers
         self.actor_optimizer = optim.Adam(
-            self.actor.parameters(), lr=pi_lr, weight_decay=2.5e-4,  eps=eps, betas=(0.99, 0.999))
-        self.actor_critic = optim.Adam(
-            self.critic.parameters(), lr=vf_lr, weight_decay=2.5e-4, eps=eps, betas=(0.99, 0.999))
+            self.actor.parameters(), lr=pi_lr, weight_decay=weight_decay,  eps=eps)
+        self.critic_optimizer = optim.Adam(
+            self.critic.parameters(), lr=vf_lr, weight_decay=weight_decay, eps=eps)
 
         # Save parameters
         self.gamma = gamma
@@ -183,9 +195,15 @@ class PPO:
         self.environment_name = environment_name
         self.actorGradientNormalization = actorGradientNormalization
         self.observationNormalization = observationNormalization
+        self.eps = eps
+        self.coeficient_entropy = coeficient_entropy
+        self.weight_decay = weight_decay
+        self.coeficient_value = coeficient_value
+        self.lmbda = lmbda
+        self.advantageAlgorithm = advantageAlgorithm
 
         # Loss
-        self.mseLoss = nn.MSELoss()
+        self.mseLoss = nn.L1Loss()
 
     def update(self, memory, writer):
 
@@ -200,10 +218,10 @@ class PPO:
                 old_states, old_actions, self.actor)
 
             # GAE Compute rewards
-            rewards, advantages = self.gae(self.gamma, memory, values)
-
-            # Default compute rewards
-            #rewards, advantages = self.compute_returns(self.gamma, memory)
+            if self.advantageAlgorithm == "GAE":
+                rewards, advantages = self.gae(self.gamma, memory, values)
+            else:
+                rewards, advantages = self.compute_returns(self.gamma, memory)
 
             # Finding the ratio (pi_theta / pi_theta__old):
             ratios = torch.exp(logprobs - old_logprobs)
@@ -212,24 +230,25 @@ class PPO:
             surr2 = torch.clamp(ratios, 1 - self.clip_ratio,
                                 1 + self.clip_ratio) * advantages
             policy_loss = -torch.min(surr1, surr2)
-            value_loss = coeficient_value * self.mseLoss(values, rewards)
+            value_loss = self.coeficient_value * \
+                self.mseLoss(values, rewards)
             # entropy del critic or actor?
-            entropy = coeficient_entropy * dist_entropy
+            entropy = self.coeficient_entropy * dist_entropy
             loss = policy_loss + value_loss - entropy
 
             writer.add_scalar("policy_loss", policy_loss.mean())
             writer.add_scalar("value_loss", value_loss.mean())
-            writer.add_scalar("entropy", entropy.mean())
+            writer.add_scalar("entropy", dist_entropy.mean())
             writer.add_scalar("loss", loss.mean())
 
             self.actor_optimizer.zero_grad()
-            self.actor_critic.zero_grad()
+            self.critic_optimizer.zero_grad()
             loss.mean().backward()
             if (self.actorGradientNormalization != 0):
                 torch.nn.utils.clip_grad_norm_(
                     self.actor.parameters(), self.actorGradientNormalization)
             self.actor_optimizer.step()
-            self.actor_critic.step()
+            self.critic_optimizer.step()
 
         # Copy new weights into old policy:
         self.actor_old.load_state_dict(self.actor.state_dict())
@@ -245,7 +264,7 @@ class PPO:
         for reward, is_terminal in zip(reversed(memory.rewards), reversed(memory.is_terminals)):
             if is_terminal:
                 R = 0
-            R = reward + gamma * R * lmbda
+            R = reward + gamma * R * self.lmbda
             returns.insert(0, R)
 
         # Normalizing the returns:
@@ -260,13 +279,13 @@ class PPO:
         adv = []
         values = (v.data).cpu().numpy()
         for reward, is_terminal, value in zip(reversed(memory.rewards), reversed(memory.is_terminals), reversed(values)):
-            mask = not is_terminal
-            delta = reward + gamma * value * mask - value
-            gae = delta + gamma * lmbda * mask * gae
+            done_mask = not is_terminal
+            delta = reward + gamma * value * done_mask - value
+            gae = delta + gamma * self.lmbda * done_mask * gae
             returns.insert(0, gae + value)
             adv.insert(0, gae)
 
-        adv = (adv - np.mean(adv)) / (np.std(adv) + eps)
+        adv = (adv - np.mean(adv)) / (np.std(adv) + self.eps)
         returns = torch.tensor(returns, dtype=torch.float32).to(device)
         adv = torch.tensor(adv, dtype=torch.float32).to(device)
         return returns, adv
@@ -278,6 +297,11 @@ def prodOfTupple(val):
     for ele in val:
         res *= ele
     return res
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
 
 
 def normalizeArray(data):
@@ -302,7 +326,17 @@ def train(environment_name,
           pathForBasePolicyToTrain=None,
           pathForBaseCriticToTrain=None,
           observationNormalization=False,
-          actorGradientNormalization=0):
+          actorGradientNormalization=0,
+          eps=None,
+          coeficient_entropy=0.01,
+          weight_decay=1e-4,
+          coeficient_value=0.5,
+          lmbda=0.95,
+          initialization="orthogonal",
+          advantageAlgorithm="GAE",
+          saveModelsEvery=10_000,
+          tensorboardName=None
+          ):
 
     previousTrainedPolicy = None
     if pathForBasePolicyToTrain:
@@ -324,8 +358,14 @@ def train(environment_name,
     if (posibles_actions == None):
         posibles_actions = env.action_space.n
 
+    if eps == None:
+        eps = np.finfo(np.float32).eps.item()
+
     # Setup tensorboard
-    writer = SummaryWriter('board/ppo')
+    if tensorboardName == None:
+        tensorboardName = datetime.now().strftime("%Y%m%d%H%M")
+    writer = SummaryWriter(
+        f'board/ppo/{environment_name}/{tensorboardName}')
 
     # Random
     if random_seed:
@@ -349,11 +389,20 @@ def train(environment_name,
               previousTrainedPolicy=previousTrainedPolicy,
               previousTrainedCritic=previousTrainedCritic,
               observationNormalization=observationNormalization,
-              actorGradientNormalization=actorGradientNormalization)
+              actorGradientNormalization=actorGradientNormalization,
+              eps=eps,
+              coeficient_entropy=coeficient_entropy,
+              weight_decay=weight_decay,
+              lmbda=lmbda,
+              coeficient_value=coeficient_value,
+              initialization=initialization,
+              advantageAlgorithm=advantageAlgorithm,
+              )
 
     startTime = time.time()
     for i_episode in range(1, max_episode_length+1):
         state = env.reset()
+        episode_reward = 0
         for t in range(1, max_steps + 1):
             timestep += 1
             # normalize state
@@ -373,12 +422,19 @@ def train(environment_name,
                 ppo.update(memory, writer)
 
             running_reward += reward
+            episode_reward += reward
+
+            writer.add_scalar('lr actor', get_lr(ppo.actor_optimizer))
+            writer.add_scalar('lr critic', get_lr(ppo.critic_optimizer))
+
             if render:
                 env.render()
             if done:
                 break
 
         avg_length += t
+
+        writer.add_scalar('episode_reward', int(episode_reward))
 
         writer.add_scalar('running_reward', int(
             (running_reward / log_interval)))
@@ -396,8 +452,8 @@ def train(environment_name,
         if solved_reward < x:
             print("-----> goal archived, stop the training")
             break
-        if i_episode % 10_000 == 0:
-            numK = i_episode / 1_000
+        if i_episode % saveModelsEvery == 0:
+            numK = i_episode // 1_000
             torch.save(ppo.actor_old.state_dict(),
                        './model/ppo_{}_policy_{}K.pth'.format(environment_name, numK))
             torch.save(ppo.critic_old.state_dict(),
@@ -408,6 +464,7 @@ def train(environment_name,
     torch.save(ppo.critic_old.state_dict(),
                './model/ppo_{}_critic_latest.pth'.format(environment_name))
     env.close()
+    writer.close()
     return ppo
 
 
