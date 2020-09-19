cc5a50840bc7b5f9d4417a6c114990bc82acd2e3
Abraham Alcaina <a.alcaina@flexidao.com>
2020-09-19 10:43:55 +0200

    feat reduce the number of neurons fix play bug feat load initial model feat finish on solved reward chore prints when train

--- src/ppo/breakoutasync.py
+++ src/ppo/breakoutasync.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
         m = asyncppo.Main(
             gamma=0.99,
             lmda=0.95,
-            n_update=5_000,
+            n_update=10_000 * 10,
             n_epochs=4,
-            n_workers=10,
-            worker_steps=512,
+            n_workers=12,
+            worker_steps=32,
             n_mini_batch=4,
             lr=2.5e-4,
             coeficient_entropy=0.01,
@@ -16,8 +16,11 @@ if __name__ == "__main__":
             actorGradientNormalizations=0.5,
             game='BreakoutNoFrameskip-v4',
             log_interval=10,
+            solved_reward=250,
+            base_model_path="./model/ppo_BreakoutNoFrameskip-v4_100k_F_.pth"
         )
         m.run_training_loop()
         m.destroy()
-    asyncppo.play(game='BreakoutNoFrameskip-v4',
-                  model_file='ppo_BreakoutNoFrameskip-v4_0K.pth',  plot=True)
+    asyncppo.play_latest(game='BreakoutNoFrameskip-v4', plot=True)
+    # asyncppo.play(game='BreakoutNoFrameskip-v4',
+    #               model_file="ppo_BreakoutNoFrameskip-v4_100k_F_.pth", plot=True)
